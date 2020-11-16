3228df8d1eb953a98ab0c0397ed351eb93e92ab1
Zirafnik <dh@ptica.com>
2020-11-16 18:38:33 +0100

    bugfix: matrices not immidiately changing grid size  + start button gets removed after click

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,26 @@
 const DOM = (function() {
     const elements= {
+        div1: document.querySelector('#div1'),
+
         start: document.querySelector('#start'),
+        
 
         inputContainers: document.querySelectorAll('.inputContainer'),
         container1: document.querySelector('#container1'),
         container2: document.querySelector('#container2'),
 
         DSizeInput: document.querySelector('#Dmatrix-size'),
-        LSizeInput: document.querySelector('#Tmatrix-size'),
+        TSizeInput: document.querySelector('#Tmatrix-size'),
     }
 
     //EVENT LISTENERS
+    //START
     elements.start.addEventListener('click', function() {
         cleanInputContainers();
-        createMatrix(storage.Dlength, elements.container1);
-        createMatrix(storage.Tlength, elements.container2);
+        createMatrix(storage.getDlength(), elements.container1);
+        createMatrix(storage.getTlength(), elements.container2);
+        //removes start button
+        elements.div1.removeChild(elements.start);
     });
 
     const cleanInputContainers= () => {
@@ -49,8 +55,13 @@ const DOM = (function() {
 })();
 
 const storage= (function() {
-    let Dlength= DOM.elements.DSizeInput.value;
-    let Tlength= DOM.elements.LSizeInput.value;
+    
+    const getDlength= () => {
+        return DOM.elements.DSizeInput.value;
+    }
+    const getTlength= () => {
+        return DOM.elements.TSizeInput.value;
+    }
 
     let matrixD=[];
     let matrixT=[];
@@ -64,7 +75,7 @@ const storage= (function() {
             });
             //turn array of numbers into array of arrays --> each array is 1 row, and each element in array is the column index
             while(matrix.length) {
-                matrixD.push(matrix.splice(0, Dlength));
+                matrixD.push(matrix.splice(0, getDlength()));
             }
 
         } else if(container== DOM.elements.container2) {
@@ -74,16 +85,16 @@ const storage= (function() {
             });
             //turn array of numbers into array of arrays --> each array is 1 row, and each element in array is the column index
             while(matrix.length) {
-                matrixT.push(matrix.splice(0, Tlength));
+                matrixT.push(matrix.splice(0, getTlength()));
             }
         }
     }
 
     return {
-        Dlength,
-        Tlength,
+        getDlength,
+        getTlength,
         saveMatrix,
         matrixD,
-        matrixT
+        matrixT,
     }
 })();
